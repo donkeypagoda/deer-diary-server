9e13890ecbe3998deacdcde43e8129a227095afc
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-24 15:00:03 -0700

    most basic server stable

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const express = require 'express'
+const express = require ('express')
+const bodyParser = require ('body-parser')
+
 const app = express()
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log('listening on ', port)
+})
