d2ae82df3d03d2f9dcd1e64aa40045f8b38d5de4
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-25 11:54:50 -0700

    building routes

--- routes/blogPosts.js
+++ routes/blogPosts.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const knex = require('knex')
 
-
 const router = express.Router()
 
+router.get("blog_posts/", (req, res, next) => {
+  knex("blog_posts")
+    .then(posts => {
+      res.send(posts)
+    })
+    .catch(err => next(err))
+})
 
 module.exports = router
