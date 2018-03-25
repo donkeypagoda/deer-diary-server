27e604a934d68e6782f855d0d39280c729f772b4
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-25 12:16:52 -0700

    working on seed migration handshake

--- migrations/20180325114159_blog_posts.js
+++ migrations/20180325114159_blog_posts.js
@@ -2,7 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('blog_posts', table => {
     table.increments();
-    table.timestamps(true,true)
+    table.string("timestamp") //this is probably bad form and the timestamp should happen here in the server, but I'm adding the server
+                              // to what's already built
     table.string('title')
     table.string('content')
 
