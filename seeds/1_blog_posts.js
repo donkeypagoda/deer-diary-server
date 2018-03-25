a60c0ff4a5e665e3c2c8ba8f68ea0095ae12cb33
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-25 12:10:46 -0700

    seed

--- seeds/1_blog_posts.js
+++ seeds/1_blog_posts.js
@@ -1,13 +1,33 @@
+const posts = [
+        {
+          id: 0,
+          timestamp: '2012-02-18 14:28:32',
+          title: "I ate the berries",
+          content: "berries are so good and yummy and I like them because I am a deer"
+        },
+        {
+          id: 1,
+          timestamp: '2012-02-20 16:28:32',
+          title: "There is a bear named Rodzher in the forest",
+          content: "watch out cause if he catches you he will make you listen to all of his puns and also he likes venison"
+        },
+        {
+          id: 2,
+          timestamp: '2014-02-25 12:28:32',
+          title: "Do deer like tacos?",
+          content: "of course they do, everyone loves tacos what a silly question, what are you a bear???"
+        }
+      ]
+
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('blog_posts').del()
+  .then(() => {
+    return knex('blog_posts').insert(posts);
+  })
+  .then(() => {
+    return knex.raw(
+      "SELECT setval('blog_posts_id_seq', (SELECT MAX(id) FROM blog_posts));"
+    );
+  });
 };
