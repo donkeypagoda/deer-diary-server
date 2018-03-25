7cf5f5e95db0e7462bd75752275169fa1738ec41
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-25 11:49:04 -0700

    made the migration

---
+++ migrations/20180325114159_blog_posts.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('blog_posts', table => {
+    table.increments();
+    table.timestamps(true,true)
+    table.string('title')
+    table.string('content')
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('blog_posts')
+};
