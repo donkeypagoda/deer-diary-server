8f36c20dafc107e4062c84b143abcb05fc735c0c
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-24 18:41:48 -0700

    building routes and knex

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require ('express')
 const bodyParser = require ('body-parser')
+const blogPosts = require('./routes/blogPosts')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(blogPosts)
 
 const port = process.env.PORT || 5000
+
 
 app.listen(port, () => {
   console.log('listening on ', port)
