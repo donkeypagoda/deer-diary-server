54c155dbc57e0674e3364c9c9c1b031ef8b7b204
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-28 13:05:38 -0700

    cors needs to be applied to the express middleware before the route is applied, I know that now

--- server.js
+++ server.js
@@ -4,19 +4,9 @@ const blogPosts = require('./routes/blogPosts')
 const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 app.use(blogPosts)
-// I dont' know what this is doing, I swiped it from roger's code to resolve CORS issues, but it didn't work
-//app.use(cors())
-
-//
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5000");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    next();
-});
 
 const port = process.env.PORT || 5000
 
