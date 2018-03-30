728224e674f0927fc4b989923654c4faefbd8fc7
donkeypagoda <christopherallenstewart@gmail.com>
2018-03-30 11:18:54 -0700

    ready to add post

--- routes/blogPosts.js
+++ routes/blogPosts.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get("/blog_posts", (req, res) => {
   knex('blog_posts').then((posts) => {
-      res.send(posts)
+      res.send({posts})
       console.log(posts)
     })
 })
